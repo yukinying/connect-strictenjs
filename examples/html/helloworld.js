5ed603ed4e2a6dcf44367bed3320756a66c8d9fa
Albert Yu <yukinying@gmail.com>
2012-11-07 13:11:52 +0800

    Initial commit

---
+++ examples/html/helloworld.js
@@ -0,0 +1,4 @@
+(function(){ console.log('hello world!'); document.write('hello world'); /* this is a quick and dirty... */ })();
+var b = 2;
+(function(){ b = 3; console.log("b ="+ b);})();
+(function(){ a = 1; console.log("a ="+ a);})();
