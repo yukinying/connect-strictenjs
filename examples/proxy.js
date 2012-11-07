ecaf9a43635bdcbb0e7ae1b645f9cdfb0a0185e7
Albert Yu <yukinying@gmail.com>
2012-11-07 13:11:52 +0800

    Initial commit

---
+++ examples/proxy.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var connect = require('connect');
+
+var httpProxy = require('http-proxy')
+var url = require('url');
+
+// for development, use npm link to create symlink based installation.
+var strict = require('connect-strictenjs');
+
+var proxy = new httpProxy.RoutingProxy();
+
+app = express();
+
+var proxyware = function( req, res ) { 
+  var uri = url.parse(req.url);
+  req.headers["Accept-Encoding"] = '';
+  proxy.proxyRequest(req, res, {
+    host: uri.hostname,
+    port: uri.port || 80
+  });
+};
+
+// app.use(express.logger());
+app.use(connect.responseTime());
+app.use(strict.stricten() );
+app.use(strict.beautifier());
+app.all('/*',proxyware);
+
+app.listen(8080);
---
+++ examples/static.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var connect = require('connect');
+
+var url = require('url');
+
+// for development, use npm link to create symlink based installation.
+var strict = require('connect-strictenjs');
+
+app = express();
+
+// app.use(express.logger());
+app.use(connect.responseTime());
+app.use(strict.stricten() );
+app.use(strict.beautifier());
+
+app.use(express.static(__dirname + '/html'));
+
+app.listen(8080);
